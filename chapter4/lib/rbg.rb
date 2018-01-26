3ff2775f34c2e2675804abdd5cf5eaf0c61890b4
msbn <shiiharagiken@gmail.com>
2018-01-27 02:39:20 +0900

    bugfix

--- chapter4/lib/rbg.rb
+++ chapter4/lib/rbg.rb
@@ -1,10 +1,10 @@
 # /chapter4/lib/rbg.rb
 def to_hex(r, g, b)
-  [r,g,b].inject('#'){|fst, color| fst += color.to_s(16).rjust(2, '00') }
+  [r, g, b].inject('#') { |fst, color| fst += color.to_s(16).rjust(2, '00') }
 end
 
 def to_ints(color)
-  color.scan(/[0-9A-F]{2}/i).map{|str| eval("0x#{str}") }
+  color.scan(/[0-9A-F]{2}/i).map { |str| eval("0x#{str}") }
 end
 
 __END__
